0bc235712f38d2e45a4dcf0adb6e505a09229239
James Chen <jchen9827@gmail.com>
2021-12-12 13:03:15 -0500

    Start main.py and edit computation.py
    - Edits to computation.py
       - Created a function to run all functions in Computation Module (Main function taking input from Data Module and returning output for Display Module)
          - Work in progress
       - Separated filtering capability from dict_to_x_y_coords into its own function

--- computation.py
+++ computation.py
@@ -12,11 +12,50 @@ from data import open_convert_and_aggregate
 
 
 ##########################################
-# Computation: Outputting Coefficients of Line of Best Fit
+# Computation: Main function taking input from Data Module and returning output for Display Module
 ##########################################
 
-def dict_to_x_y_coords(data: dict[str, list[tuple[tuple[int, int], int]]], sector: str,
-                       n_pred: int) -> tuple[list[tuple[int, int]], list[int]]:
+def run_computations(data: dict[str, list[tuple[tuple[int, int], int]]], n_pred: int) \
+        -> dict[str, tuple[list[int], list[int], list[float]]]:
+    """Compute and display for each sector
+    """
+    sectors = ['Primary Sector', 'Secondary Sector', 'Tertiary Sector', 'Quaternary Sector']
+    for sector in sectors:
+        x_y_coords = dict_to_x_y_coords(data=data, sector=sector, n_pred=5)
+        # x_y_coords[0]: dates
+        # x_y_coords[1]: actual GDP values used for prediction
+        slope, intercept = regress(x_y_coords)  # for line of best fit
+        deviations = calculate_dev(data[sector], slope, intercept)
+        # TODO: change dict_to_x_y_coords to only convert dict to x_y_coords and add a filtering
+        #  function (calculate_dev will receive x_y_coords as input while regression receive
+        #  filtered x_y_coords as input)
+
+
+##########################################
+# Computation: Separating and filtering x and y coordinates (for a sector)
+##########################################
+
+def dict_to_x_y_coords(data: dict[str, list[tuple[tuple[int, int], int]]], sector: str) \
+        -> tuple[list[tuple[int, int]], list[int]]:
+    """ Convert industry's data into a tuple of x-coords and y-coords.
+
+    data in format: dict[str*industry*, list[tuple[tuple[int*year*, int*month*]], int*gdp*]]]
+
+    x-coords: year
+    y-coords: gdp
+
+    >>> data = open_convert_and_aggregate('samp1.csv')  # From Jan 2012 to Sep 2021
+    >>> dict_to_x_y_coords(data, 'Primary Sector')
+
+    """
+    x_coords = [data[sector][index][0] for index in range(len(data[sector]))]
+    y_coords = [data[sector][index][1] for index in range(len(data[sector]))]
+
+    return x_coords, y_coords
+
+
+def filter_x_y_coords(data: dict[str, list[tuple[tuple[int, int], int]]], sector: str,
+                      n_pred: int) -> tuple[list[tuple[int, int]], list[int]]:
     """ Convert industry's data into a tuple of x-coords and y-coords, filtered to include n_pred
     data points.
 
@@ -46,7 +85,7 @@ def dict_to_x_y_coords(data: dict[str, list[tuple[tuple[int, int], int]]], secto
 
 def diff_month(d1: datetime.date, d2: datetime.date) -> int:
     """
-    Helper function for dict_to_x_y_coords.
+    Helper function 1:
 
     Return the number of months between d1 and d2.
 
@@ -58,18 +97,22 @@ def diff_month(d1: datetime.date, d2: datetime.date) -> int:
     return (d1.year - d2.year) * 12 + d1.month - d2.month
 
 
+##########################################
+# Computation: Calculating coefficients of line of best fit
+##########################################
+
 def regress(x_y_coords: tuple[list[tuple[int, int]], list[int]]) -> tuple[float, float]:
     """Take x_y_coords and return coefficients of the line of best fit (y = a * x + b)
     as (a, b).
 
-    >>> coefficients = regress(([0, 1, 2], [0, 2, 4]))
+    >>> coefficients = regress(([(2019, 12), (2020, 1), (2020, 2)], [0, 2, 4]))
     >>> math.isclose(round(coefficients[0], 4), 2)
     True
     >>> math.isclose(round(coefficients[1], 4), 0)
     True
     """
     model = LinearRegression()
-    x_coords = np.arange(len(x_y_coords[1])).reshape(-1, 1)
+    x_coords = np.arange(len(x_y_coords[0])).reshape(-1, 1)
     y_coords = np.array(x_y_coords[1])
 
     # Train model
@@ -77,6 +120,10 @@ def regress(x_y_coords: tuple[list[tuple[int, int]], list[int]]) -> tuple[float,
 
     return model.coef_[0], model.intercept_
 
+
+##########################################
+# Computation: Predicting GDP using line of best fit and finding deviation to actual values
+##########################################
 
 def predict_gdp() -> ...:
     """ Predict gdp using regress values
@@ -103,8 +150,7 @@ if __name__ == '__main__':
     import python_ta
 
     python_ta.check_all(config={
-        'extra-imports': ['python_ta.contracts', 'numpy', 'sklearn', 'math', 'dataclass',
-                          'datetime'],
+        'extra-imports': ['python_ta.contracts', 'numpy', 'sklearn', 'math', 'datetime'],
         'max-line-length': 100,
         'disable': ['R1705', 'C0200']
     })
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+"""
+Main Module for the final project (CSC110 at the University of Toronto)
 
+This file is Copyright (c) 2021 Xi Chen, Taymoor Farooq, Se-Eum Kim and Henry Klinck.
+"""
+
+from data import open_convert_and_aggregate
+from computation import run_computations
+
+
+def run_program(file: str = 'samp1.csv') -> None:
+    """Runs the entire program by processing file (samp1.csv by default), predicting GDP values,
+    and displaying graphs.
+
+    samp1.csv has dates from Jan 2012 to Sep 2021.
+    """
+    data = open_convert_and_aggregate(file)
+
+    data_points = run_computations(data, 5)
