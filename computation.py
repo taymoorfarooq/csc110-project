0713eacafc98c257113486d1723065120a5d54de
James Chen <jchen9827@gmail.com>
2021-12-13 17:54:50 -0500

    Update computation.py
    - Added py_ta contracts

--- computation.py
+++ computation.py
@@ -240,4 +240,9 @@ if __name__ == '__main__':
 
     import doctest
 
-    doctest.testmod()
+    doctest.testmod(verbose=True)
+
+    import python_ta.contracts
+
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
